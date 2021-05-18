c83fc71571d33446ec79102af0101a036c5e3536
Brendha Luiza Barbosa Pereira <barbosabrendhaluiza@gmail.com>
2021-05-18 16:22:11 -0300

    nada

---
+++ src/components/ItemsChosenByUser/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+class ItemsChosenByUser extends React.Component{
+
+    render(){
+        return (
+            <h1>brendha's area</h1>
+        )
+    }
+}
+
+export default ItemsChosenByUser
