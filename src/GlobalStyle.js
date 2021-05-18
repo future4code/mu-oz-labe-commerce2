7ab86a9090218d7f247c6518758834faf9851b0d
guilhermeCassettari <guizaocassettari@gmail.com>
2021-05-18 10:18:53 -0300

    Estilizaçoes do header

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -1,8 +1,25 @@
-import {createGlobalStyle} from 'styled-components'
+import { createGlobalStyle } from 'styled-components'
 
 export const backGroundBlack = '#1c1c1c'
 export const black = '#272727'
 export const white = '#EBEBEB'
+
+export const flexColumn = () => {
+    return `
+        display:flex;
+        justfy-content: center;
+        align-items: center;
+        flex-direction: column;
+    `
+}
+
+export const flexRow = () => {
+    return `
+        display:flex;
+        justfy-content: center;
+        align-items: center;    
+    `
+}
 
 const GlobalStyle = createGlobalStyle`
     body{
